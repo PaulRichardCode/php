615c9395d5ec7e12d5fbe98f728ccb6655f586d9
Paul Richard <112706825+PaulRichardCode@users.noreply.github.com>
2023-12-19 18:19:01 +0100

    first commit

---
+++ welcome.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php body</title>
+</head>
+<body>
+    <?php
+    $name = "Richard";
+    $food = "Jollof Rice";
+    $age = "23";
+    $price = 4.99;
+
+    echo"My name is {$name} and i like {$food},<br> I am {$age} years old <br>";
+    var_dump($num);
+    echo "<br>Your {$food} is ";
+   ?>
+    
+</body> 
+</html>
