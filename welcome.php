e7bdb9f1c00d5fe97ca8d4bddda77b4633960a0c
Paul Richard <112706825+PaulRichardCode@users.noreply.github.com>
2023-12-23 13:10:16 +0100

    recap succesful

--- welcome.php
+++ welcome.php
@@ -6,22 +6,16 @@
     <title>php body</title>
 </head>
 <body>
-  
-<form action="/welcome.php" method="post">
-    <label for="quantity">
-        Quantity:
-        <input type="number" name="quantity" id="quantity">
-    </label>
-    <input type="submit" value="Total   ">
-</form>
+    <form action="/welcome.php" method="get">
+        <label for="quantity">quantity:
+            <input type="number" name="number" id="number">
+        </label>
+        <input type="submit" value="Total">
+    </form>
+            
 </body> 
 </html>
 
-<?php 
-$quantity = $_POST["quantity"];
-$price = 5.99;
-$goodsType = "Belts";
-$TOTAL = $quantity * $price;
-
-echo "{$quantity} {$goodsType} costs \${$TOTAL}";
+<?php
+    echo "{$_GET["number"]}";
 ?>
