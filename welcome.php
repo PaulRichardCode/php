45e1d5330bec2a8c155c24440a3d8383b2ddf9ad
Paul Richard <112706825+PaulRichardCode@users.noreply.github.com>
2023-12-24 10:55:17 +0100

    mn

--- welcome.php
+++ welcome.php
@@ -6,16 +6,26 @@
     <title>php body</title>
 </head>
 <body>
-    <form action="/welcome.php" method="get">
-        <label for="quantity">quantity:
-            <input type="number" name="number" id="number">
-        </label>
-        <input type="submit" value="Total">
+    <form action="/welcome.php" method="post">
+       <label for="radius">Radius:</label>
+       <input type="number" name="number" id="number">
+       <input type="submit" value="calculate">
     </form>
             
 </body> 
 </html>
 
 <?php
-    echo "{$_GET["number"]}";
+   $radius = $_POST["number"];
+    $circumference = null;
+    $volume = null;
+
+   $pi = pi();
+   $circumference = $radius * 2 * pi();
+   $circumference = round($circumference,2);
+   $volume = $radius * pi() * 4/3;
+   $volume = round($volume, 2);
+
+   echo "circumference = {$circumference}cm <br>";
+   echo "volume = {$volume}"
 ?>
