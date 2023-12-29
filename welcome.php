013abbecf7af0c4509f43aa7c610983bdae7e0b7
Paul Richard <112706825+PaulRichardCode@users.noreply.github.com>
2023-12-29 13:05:57 +0100

    php is very simple lol

--- welcome.php
+++ welcome.php
@@ -7,26 +7,12 @@
 </head>
 <body>
     <form action="welcome.php" method="post">
-        <input type="checkbox" name="food" id="pizza" value="PIZZA"> pizza: <br>
-        <input type="checkbox" name="food" id="Hamburger" value="HAMBURGER"> Hamburger: <br>
-        <input type="checkbox" name="food" id="Hotdog" value="HOTDOG"> Hotdog: <br>
-        <input type="checkbox" name="food" id="Taco" value="TACO"> Taco: <br>
+        <input type="checkbox" name="foods[]" id="pizza" value="PIZZA"> pizza: <br>
+        <input type="checkbox" name="foods[]" id="Hamburger" value="HAMBURGER"> Hamburger: <br>
+        <input type="checkbox" name="foods[]" id="Hotdog" value="HOTDOG"> Hotdog: <br>
+        <input type="checkbox" name="foods[]" id="Taco" value="TACO"> Taco: <br>
         <input type="submit" value="submit" name="submit">
     </form>
 </body>
 </html>             
 
-<?php
-
-    if(isset($_POST["submit"])) {
-       
-        if(isset($_POST["food"])) {
-            $food = $_POST["food"];
-            echo $food;
-        }
-    }
-
-
-
-
-?>
