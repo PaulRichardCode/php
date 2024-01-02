78ddc8f5e3d834667c3aaf1ea0faaf4918f3f3b3
Paul Richard <112706825+PaulRichardCode@users.noreply.github.com>
2024-01-02 16:03:38 +0100

    sql aint easy

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <?php 
         include("database.php");
+
+        $sql = "INSERT INTO users(user, password)
+                VALUES('Richard', '123Rich')";
+
+        mysqli_query($conn, $sql);
+
+        mysqli_close($conn);
 ?>
