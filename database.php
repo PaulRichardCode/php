a2034c5600456a9718bb0fb2c00f0a2b18b3b0bf
Paul Richard <112706825+PaulRichardCode@users.noreply.github.com>
2024-01-02 15:15:34 +0100

    php

--- database.php
+++ database.php
@@ -1,20 +1,68 @@
 <?php 
+
+    include("database.php");
     $db_server = "localhost";
     $db_user = "root";
     $db_pass = "";
-    $db_name = "businessdb";
+    $db_name = "storagedb";
     $conn = "";
 
-   
+    
+
 
     try {
         $conn = mysqli_connect($db_server, $db_user, $db_pass, $db_name);
-    } catch(mysqli_sql_exception) {
-        echo "Could not connect";
+    } catch (mysqli_sql_exception) {
+        echo "you are wasting time";
     }
 
-    if($conn){
-        echo "You are connected";
+    if($conn) {
+        echo "It don connect";
     } 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // $db_server = "localhost";
+    // $db_user = "root";
+    // $db_pass = "";
+    // $db_name = "businessdb";
+    // $conn = "";
+
+   
+
+    // try {
+    //     $conn = mysqli_connect($db_server, $db_user, $db_pass, $db_name);
+    // } catch(mysqli_sql_exception) {
+    //     echo "Could not connect";
+    // }
+
+    // if($conn){
+    //     echo "You are connected";
+    // } 
 
 ?>
