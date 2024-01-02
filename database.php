1b18d2ba287bca8ca45d5fc10dd241d05b2707b4
Paul Richard <112706825+PaulRichardCode@users.noreply.github.com>
2024-01-02 07:59:44 +0100

    working on database

--- database.php
+++ database.php
@@ -5,8 +5,16 @@
     $db_name = "businessdb";
     $conn = "";
 
-    $conn = mysqli_connect($db_server, $db_user, $db_pass, $db_name)
+   
 
-    
+    try {
+        $conn = mysqli_connect($db_server, $db_user, $db_pass, $db_name);
+    } catch(mysqli_sql_exception) {
+        echo "Could not connect";
+    }
+
+    if($conn){
+        echo "You are connected";
+    } 
 
 ?>
