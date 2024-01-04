2ebb6f2fa550d394994283e72b91c5431be05d2a
Paul Richard <112706825+PaulRichardCode@users.noreply.github.com>
2024-01-04 21:04:40 +0100

    lobatan wakatime no count o. eithers im learning more

--- index.php
+++ index.php
@@ -18,15 +18,13 @@
         // }
        
         
-        $username = "Thomas Shelby";
-        $password = "godloveme123";
+      $sql = "SELECT * FROM users WHERE user = 'spongebob'";
+      $result = mysqli_query($conn, $sql);
 
-        $hash = password_hash($password, PASSWORD_DEFAULT);
-        
-        $sql = "INSERT INTO users(user, password)
-                VALUE('$username', '$hash')";
-        
-        mysqli_query($conn, $sql);
+    if(mysqli_num_rows($result) > 0) {
+        $rows =mysqli_fetch_assoc($result);
+        echo $rows["id"] . '<br>' ;
+    }
 
         mysqli_close($conn);
 ?>
