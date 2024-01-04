2075804d7ccafb2934928d770c7680c8dedbb254
Paul Richard <112706825+PaulRichardCode@users.noreply.github.com>
2024-01-04 20:31:35 +0100

    do me kin kuu

--- index.php
+++ index.php
@@ -8,15 +8,21 @@
         // $sql = "INSERT INTO users(user, password)
         //         VALUES('$username', '$hash')";
 
-        $sql = "SELECT * FROM users WHERE user = 'spongebob'";
+        // $sql = "SELECT * FROM users WHERE user = 'spongebob'";
 
-        try {
-            mysqli_query($conn, $sql);
-            echo "user is now registered";
-        } catch (mysqli_sql_exception) {
-            echo "Could not register user";
-        }
+        // try {
+        //     mysqli_query($conn, $sql);
+        //     echo "user is now registered";
+        // } catch (mysqli_sql_exception) {
+        //     echo "Could not register user";
+        // }
        
+        
+        $username = "Thomas Shelby";
+        $password = "godloveme123";
+
+        $hash = password_hash($password, PASSWORD_DEFAULT);
+        echo "$username" . "$hash";
 
         mysqli_close($conn);
 ?>
