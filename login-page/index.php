2d12b3be4fa5772db02cabc8de78f29736238f72
Paul Richard <112706825+PaulRichardCode@users.noreply.github.com>
2024-01-06 11:26:55 +0100

    login project begining

---
+++ login-page/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Login page</title>
+</head>
+<body>
+    <div class="container">
+        <h1>Log in</h1>
+        <form action="index.php" method="post" class="form">
+            <label for="username">UserName:</label>
+            <input type="text" name="username" placeholder="username" id="username">
+            <label for="password">Password:</label>
+            <input type="password" name="password" placeholder="****************" id="password">
+            <input type="submit" value="Login" class="btn">
+        </form>
+
+    </div>
+
+</body>
+</html>
