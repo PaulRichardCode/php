c66900cd6494b85491883706840abe3c1a102594
Paul Richard <112706825+PaulRichardCode@users.noreply.github.com>
2024-01-09 15:52:42 +0100

    it works well for now

--- admin-page/index.php
+++ admin-page/index.php
@@ -17,7 +17,7 @@
     ?>
  <div class="container">
  <h1>ADD A NEW PRODUCT</h1>
-       <form action="./index.php" method="post">
+       <form action="./index.php" method="post" class="form1">
         <input type="text" name="product" placeholder="enter the product name" id="name">
         <input type="number" name="price" placeholder="enter the product price" id="price">
         <input type="file" name="file" id="file">
